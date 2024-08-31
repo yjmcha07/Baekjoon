d709ac6b92021aa5fb30fad966e7a972f8ef8d77
yjmcha07 <yjmcha07@gmail.com>
2024-08-31 10:20:56 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,10 @@
+a,b=map(int, input(). split())
+basket=[]
+for i in range (1,a+1):
+    basket.append(i)
+for i in range (b):
+    x,y = map(int, input(). split())
+    temp=basket[x-1]
+    basket[x-1]=basket[y-1]
+    basket[y-1]=temp
+print(*basket)

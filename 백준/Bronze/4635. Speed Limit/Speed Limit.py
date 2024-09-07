6f2a03546a38bcd286ef7349c447d82fb77488a1
yjmcha07 <yjmcha07@gmail.com>
2024-09-07 10:34:34 +0900

    [Bronze III] Title: Speed Limit, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4635. Speed Limit/Speed Limit.py
@@ -0,0 +1,13 @@
+while True:
+    a=int(input())
+    k=[]
+    c1=0
+    if a==-1:
+        break
+    elif a>0:
+        for i in range (a):
+            b,c=map(int, input(). split())
+            k.append(b*(c-c1))
+            c1=c 
+        print(str(sum(k))+ ' miles')
+
